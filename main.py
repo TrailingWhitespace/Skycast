9c475466057ecb17081f91113b59bf23774bf5a4
TrailingWhitespace <flash1719@protonmail.com>
2025-03-07 16:07:57 +0530

    basic home page and api call setup

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import requests
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World!"
+    return render_template("home.html")
+
+@app.route("/weather", methods = ["POST"])
+def weather():
+    if request.method == "POST":
+
+        with open("api_key.txt", "r") as file:
+            api_key = file.read()
+            print(api_key)
+        city = request.form.get("query")
+        
+        base_url = "https://api.openweathermap.org/data/2.5/weather"
+        params = {
+            "q": city,           
+            "appid": api_key,    
+            "units": "metric"     
+        }
+        resp = requests.get(base_url, params=params)
+        if resp.status_code == 200:
+            return render_template("weather.html")
+        else:
+            return f"Error: {resp.status_code}, {resp.text}"
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port = 8000)
